4f3843c566f9c237cbf0a4bacb1088edb2b4d807
victordelval <victor.val.andres@gmail.com>
2018-03-18 11:34:45 +0100

    Adds initial server

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import express from 'express'
+
+let _server
+
+const server = {
+    start() {
+        const app = express()
+
+        _server = app.listen('9000', () => {
+            if (process.env.NODE_ENV !== 'test') {
+                console.log('Server listening http://localhost:9000')
+            }
+        })
+
+    },
+
+    close() {
+        _server.close()
+    }
+}
+
+export default server
+
+if (!module.parent) {
+    server.start()
+}
